b3a198f65c4fb702197f948559d95e04986b6350
Noboru Saito <noborusai@gmail.com>
2019-01-14 13:00:41 +0900

    update 0.5.0

--- trdsql.rb
+++ trdsql.rb
@@ -1,9 +1,9 @@
 class Trdsql < Formula
   desc "Tool to run the SQL query to CSV and LTSV and JSON"
   homepage "https://github.com/noborus/trdsql/"
-  url "https://github.com/noborus/trdsql/archive/v0.4.1.tar.gz"
-  version "0.4.1"
-  sha256 "17aab53f4384824ea9a386f240a20b770bbbdb8c521383558febc998fad966ef"
+  url "https://github.com/noborus/trdsql/archive/v0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "9d3d9e3e2b4e38ab378449c4be70f88e626612fe2502630dd5ecb75566e42e1b"
   depends_on "dep" => :build
   depends_on "go" => :build
   def install
