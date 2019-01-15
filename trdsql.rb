8ab9dcd095bd62cd69a7507f5bf8ff80aea1a330
Noboru Saito <noborusai@gmail.com>
2019-01-15 14:27:37 +0900

    delete line

--- trdsql.rb
+++ trdsql.rb
@@ -2,7 +2,6 @@ class Trdsql < Formula
   desc "Tool to run the SQL query to CSV and LTSV and JSON"
   homepage "https://github.com/noborus/trdsql/"
   url "https://github.com/noborus/trdsql/archive/v0.5.0.tar.gz"
-  version "0.5.0"
   sha256 "9d3d9e3e2b4e38ab378449c4be70f88e626612fe2502630dd5ecb75566e42e1b"
   depends_on "dep" => :build
   depends_on "go" => :build
