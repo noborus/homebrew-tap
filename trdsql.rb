1bec91b4a5ed4d7ffebc5a789d0c40f80121cdc7
Noboru Saito <noborusai@gmail.com>
2019-06-23 18:15:08 +0900

    update

--- trdsql.rb
+++ trdsql.rb
@@ -3,12 +3,13 @@ class Trdsql < Formula
   homepage "https://github.com/noborus/trdsql/"
   url "https://github.com/noborus/trdsql/archive/v0.6.1.tar.gz"
   sha256 "d1fb4c15f8abedb16a3549290a6ff98e9979f7b5dfa678146660edad84dd54e3"
-  depends_on "dep" => :build
   depends_on "go" => :build
   def install
     ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"]="on"
     (buildpath/"src/github.com/noborus/trdsql").install buildpath.children
     cd "src/github.com/noborus/trdsql" do
+      system "go", "mod", "download"
       system "make", "build"
       bin.install "trdsql"
     end
