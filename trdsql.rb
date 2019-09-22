f5d61ef43c10fdfd2032c0c97a678308bb27ab07
Noboru Saito <noborusai@gmail.com>
2019-09-23 08:22:26 +0900

    v0.7.1

--- trdsql.rb
+++ trdsql.rb
@@ -1,8 +1,8 @@
 class Trdsql < Formula
   desc "Tools for executing SQL queries to CSV, LTSV and JSON"
   homepage "https://github.com/noborus/trdsql/"
-  url "https://github.com/noborus/trdsql/archive/v0.6.3.tar.gz"
-  sha256 "0253a2701e614694f313f48a3a334b229e2afc051320a49a59d9461a14264900"
+  url "https://github.com/noborus/trdsql/archive/v0.7.1.tar.gz"
+  sha256 "f498f2b6302f1f99489105a833615124dc2a9c8eb2a6fe20e6c9d39434dd5371"
   depends_on "go" => :build
   def install
     ENV["GOPATH"] = buildpath
