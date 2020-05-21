aa195dc965330fd29d386bc67d8cf82fd77ec913
Noboru Saito <noborusai@gmail.com>
2020-05-21 11:21:51 +0900

    Updated trdsql to v0.7.6

--- trdsql.rb
+++ trdsql.rb
@@ -1,15 +1,15 @@
 class Trdsql < Formula
   desc "Tools for executing SQL queries to CSV, LTSV and JSON"
   homepage "https://github.com/noborus/trdsql/"
-  version "0.7.5"
+  version "0.7.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/noborus/trdsql/releases/download/v0.7.5/trdsql_v0.7.5_darwin_amd64.zip"
-    sha256 "aa794f7484c606aa0b0f1fbe87a235790f5073874fea6d43907d1081de24d14c"
+    url "https://github.com/noborus/trdsql/releases/download/v0.7.6/trdsql_v0.7.6_darwin_amd64.zip"
+    sha256 "b0e36c1fdc76c002347701987bd3006b14842ef8b69e3dcf307bc3288087d050"
   elsif OS.linux?
-    url "https://github.com/noborus/trdsql/releases/download/v0.7.5/trdsql_v0.7.5_linux_amd64.zip"
-    sha256 "343471ceb0bb4415bbb4ba1d7aeec0ebf4300ddae8c942957feadb77149d6be8"
+    url "https://github.com/noborus/trdsql/releases/download/v0.7.6/trdsql_v0.7.6_linux_amd64.zip"
+    sha256 "09b7b341a009400c4f57dc436b4822ce2f38ae486e868dcea224850901dab709"
   end
 
   def install
