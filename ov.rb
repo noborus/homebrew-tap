0e12748ab3da1340bffa4782ef67599f1908636b
noborus <noborusai@gmail.com>
2020-05-29 14:16:13 +0900

    Brew formula update for ov version v0.2.1

--- ov.rb
+++ ov.rb
@@ -2,24 +2,24 @@
 class Ov < Formula
   desc "ov is a feature rich terminal pager."
   homepage "https://github.com/noborus/ov"
-  version "0.2.0"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/noborus/ov/releases/download/v0.2.0/ov_0.2.0_darwin_amd64.zip"
-    sha256 "02f2030e3f7cd46f8de561352eaf7c90dc1af26582e5e0e926df0838722e5060"
+    url "https://github.com/noborus/ov/releases/download/v0.2.1/ov_0.2.1_darwin_amd64.zip"
+    sha256 "1003b82bca4338d078a188c375b531e7af6e54d72af7799bc9f8ec4204cae733"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/ov/releases/download/v0.2.0/ov_0.2.0_linux_amd64.zip"
-      sha256 "d9b85f4cfe2f91a9641413906e6c5d216c6fb6e715ee38fe4e79c42980319201"
+      url "https://github.com/noborus/ov/releases/download/v0.2.1/ov_0.2.1_linux_amd64.zip"
+      sha256 "e58115c1e9cb384887d968c3df1b1c515561827dab6dfbf8fe6672e2c295d595"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/noborus/ov/releases/download/v0.2.0/ov_0.2.0_linux_arm64.zip"
-        sha256 "882b31493ca37e4e4ec9534dff6480b3f88d65854550eab6e3c198e0aa1c35cb"
+        url "https://github.com/noborus/ov/releases/download/v0.2.1/ov_0.2.1_linux_arm64.zip"
+        sha256 "24240fb98dfffbc0e3e1726a34add9974780f4e1562a7102ba76bbc3a92c4c4f"
       else
-        url "https://github.com/noborus/ov/releases/download/v0.2.0/ov_0.2.0_linux_arm.zip"
-        sha256 "ea054fc44334692d1e8ca0d46d63f2d73b538f60b1a043c229357c9fd4c7ceb5"
+        url "https://github.com/noborus/ov/releases/download/v0.2.1/ov_0.2.1_linux_arm.zip"
+        sha256 "20e48b72226e125ad540873bba7b2a98de002b6c649a3cedf0f1fdfd23fda609"
       end
     end
   end
