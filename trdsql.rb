4dd355dbef028e8d7fbeda6ad5c4ee0aea664ae4
Noboru Saito <noborusai@gmail.com>
2022-05-04 11:01:33 +0900

    Fix version and add arm64

--- trdsql.rb
+++ trdsql.rb
@@ -1,18 +1,38 @@
 class Trdsql < Formula
   desc "Tools for executing SQL queries to CSV, LTSV and JSON"
   homepage "https://github.com/noborus/trdsql/"
-  version "0.9.1"
+  version "0.10.0"
 
-  if OS.mac?
-    url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_darwin_amd64.zip"
-    sha256 "412082dc8c107acf6279770aa09c17e1facdaa07f8cde63fff95b66cf9aff25f"
-  elsif OS.linux?
-    url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_linux_amd64.zip"
-    sha256 "b1a4a1d2700870f2ba201cdad1e23d92b16c3c2fea8977a783dd1662500cf37a"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_darwin_arm64.zip"
+      sha256 "803678fdbca6faed6da25fc02ba6caba0dba81d612781ba388728901853e12ab"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_darwin_amd64.zip"
+      sha256 "412082dc8c107acf6279770aa09c17e1facdaa07f8cde63fff95b66cf9aff25f"
+      def install
+        bin.install "trdsql"
+      end
   end
 
-  def install
-    bin.install "trdsql"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_linux_arm64.zip"
+      sha256 "1f2b534840faed45f1ab1f56257e13e22aa0d1ba80678e5a8724cc5d29c18e12"
+      def install
+        bin.install "trdsql"
+      end
+    end
+
+    if Hardware::CPU.intel?
+      url "https://github.com/noborus/trdsql/releases/download/v0.10.0/trdsql_v0.10.0_linux_amd64.zip"
+      sha256 "b1a4a1d2700870f2ba201cdad1e23d92b16c3c2fea8977a783dd1662500cf37a"
+
+      def install
+        bin.install "trdsql"
+      end
+    end
   end
 
   test do
