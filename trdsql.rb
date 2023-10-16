ab9f4b97a4ac56d35f7a81332037d185d6060bd3
Noboru Saito <noborusai@gmail.com>
2023-10-16 23:19:00 +0000

    Brew formula update for trdsql

--- trdsql.rb
+++ trdsql.rb
@@ -1,19 +1,19 @@
 class Trdsql < Formula
-  desc "Tools for executing SQL queries to CSV, LTSV and JSON"
+  desc "Tools for executing SQL queries to CSV, LTSV, JSON and TBLN"
   homepage "https://github.com/noborus/trdsql/"
-  version "0.12.0"
+  version  "0.12.1-rc1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/noborus/trdsql/releases/download/v0.12.0/trdsql_v0.12.0_darwin_arm64.zip"
-      sha256 "fa033118104e50989a52d004e42b0f4eb9936b317dc650322732c254a8b6c9a2"
+      url "https://github.com/noborus/trdsql/releases/download/v0.12.1-rc1/trdsql_v0.12.1-rc1_darwin_arm64.zip"
+      sha256 "6499abd608f3db076e5f4df65d0174bc1950c43e6b422db3c4033f904d9a0c43"
       def install
         bin.install "trdsql"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/trdsql/releases/download/v0.12.0/trdsql_v0.12.0_darwin_amd64.zip"
-      sha256 "977c8652f8cdd6868d68d25dd84e16f519e8e628c9183937b5cb66dadc840e5a"
+      url "https://github.com/noborus/trdsql/releases/download/v0.12.1-rc1/trdsql_v0.12.1-rc1_darwin_amd64.zip"
+      sha256 "035daa99d93eb5111fa639e24e6b2efe27e2537fb360e3f110f08d2a3344a3d1"
       def install
         bin.install "trdsql"
       end
@@ -22,16 +22,16 @@ class Trdsql < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/noborus/trdsql/releases/download/v0.12.0/trdsql_v0.12.0_linux_arm64.zip"
-      sha256 "7707f718f5618da4cf7e28966d85aadb7d0053aa18d9d39082f49d21325d8cec"
+      url "https://github.com/noborus/trdsql/releases/download/v0.12.1-rc1/trdsql_v0.12.1-rc1_linux_arm64.zip"
+      sha256 "4f89daf1bdee40ca485a1d6046cf397a2de4642c470442ccda51ec9b41b010f0"
       def install
         bin.install "trdsql"
       end
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/trdsql/releases/download/v0.12.0/trdsql_v0.12.0_linux_amd64.zip"
-      sha256 "cb71d601a575f67f5cbf0f891e2722f5f004e81b7206856ac747b59e3417000c"
+      url "https://github.com/noborus/trdsql/releases/download/v0.12.1-rc1/trdsql_v0.12.1-rc1_linux_amd64.zip"
+      sha256 "c93f48c53b42f9e4c7824d6bae98ec3d39491f1cd7fd6c8d77672ef8cad4f873"
       def install
         bin.install "trdsql"
       end
