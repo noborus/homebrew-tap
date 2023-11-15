0dc8cf7fd50856a076e5fc0a393385122bf7ed35
noborus <noborusai@gmail.com>
2023-11-15 13:21:53 +0900

    Brew formula update for xlsxsql version v0.2.0

--- xlsxsql.rb
+++ xlsxsql.rb
@@ -5,20 +5,20 @@
 class Xlsxsql < Formula
   desc "Execute SQL to xlsx and convert to other format"
   homepage "https://github.com/noborus/xlsxsql"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/xlsxsql/releases/download/v0.1.0/xlsxsql_Darwin_x86_64.tar.gz"
-      sha256 "3cd39724195bc16372f0a71f8b26afdb91f2328400a1d20aa592d9a47419f0af"
+      url "https://github.com/noborus/xlsxsql/releases/download/v0.2.0/xlsxsql_0.2.0_Darwin_x86_64.tar.gz"
+      sha256 "ecfa73b04772153f586f2f524cdbf131abf9f1ea63bab6720cd1b0e8bfdcd36c"
 
       def install
         bin.install "xlsxsql"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/noborus/xlsxsql/releases/download/v0.1.0/xlsxsql_Darwin_arm64.tar.gz"
-      sha256 "2be45b1dc27d17e23c1eb3236e1ddddc8d0bb7aa50099b931e54fdc9994e34b9"
+      url "https://github.com/noborus/xlsxsql/releases/download/v0.2.0/xlsxsql_0.2.0_Darwin_arm64.tar.gz"
+      sha256 "6a03219c579a23226d0013a41ad197411a79de44df3f4a433c1fcf892476d614"
 
       def install
         bin.install "xlsxsql"
@@ -28,16 +28,16 @@ class Xlsxsql < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/noborus/xlsxsql/releases/download/v0.1.0/xlsxsql_Linux_arm64.tar.gz"
-      sha256 "18abbda46dca0e009421e4ad2fac49981ac1ccf41d443cf54ef207870e5b6212"
+      url "https://github.com/noborus/xlsxsql/releases/download/v0.2.0/xlsxsql_0.2.0_Linux_arm64.tar.gz"
+      sha256 "e8c6abb607c43a7f6f5c17bfe092f26228c7e5b6357c84ce7abc2f1c5e04b13e"
 
       def install
         bin.install "xlsxsql"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/xlsxsql/releases/download/v0.1.0/xlsxsql_Linux_x86_64.tar.gz"
-      sha256 "51c766caceeebd9b8e9985b87b760a971e0467591de2bcc2d43e6b4c262138ec"
+      url "https://github.com/noborus/xlsxsql/releases/download/v0.2.0/xlsxsql_0.2.0_Linux_x86_64.tar.gz"
+      sha256 "ba8786d75d8dab7a00d2251a73474781bc67a21eea81f9d517df7ab0fae706d8"
 
       def install
         bin.install "xlsxsql"
