89682fa7b8a382fb6bd6da862d3dbb875701c0ba
noborus <noborusai@gmail.com>
2024-02-28 17:17:24 +0900

    Brew formula update for mdviewer version v0.1.1

--- mdviewer.rb
+++ mdviewer.rb
@@ -5,20 +5,20 @@
 class Mdviewer < Formula
   desc "markdown viewer"
   homepage "https://github.com/noborus/mdviewer"
-  version "0.1.1-alpha.2"
+  version "0.1.1"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1-alpha.2/mdviewer_0.1.1-alpha.2_darwin_arm64.zip"
-      sha256 "a68502b629ae9793003d501eee99b78e2d4b17679a6bce4cdf5e9c9e72e4a22c"
+    if Hardware::CPU.intel?
+      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1/mdviewer_0.1.1_darwin_amd64.zip"
+      sha256 "b859f0052cf8a2980b32fc3a608d24e89a0ee3f4c286634a483d9b7ce96047d4"
 
       def install
         bin.install "mdviewer"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1-alpha.2/mdviewer_0.1.1-alpha.2_darwin_amd64.zip"
-      sha256 "8f4224c90f0d2b1e09cd84343730697b5e3f4fda8fb88554eabc752e2606cf94"
+    if Hardware::CPU.arm?
+      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1/mdviewer_0.1.1_darwin_arm64.zip"
+      sha256 "2f8350fdc17a10b420906d3fe41ccc14eb7649b92b1c579b76ca009b44e543b3"
 
       def install
         bin.install "mdviewer"
@@ -28,24 +28,24 @@ class Mdviewer < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1-alpha.2/mdviewer_0.1.1-alpha.2_linux_arm.zip"
-      sha256 "4e2cc32c2573a6f72253bc3545696a0419eff839ab6009bee1942f240d3a42f7"
-
-      def install
-        bin.install "mdviewer"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1-alpha.2/mdviewer_0.1.1-alpha.2_linux_arm64.zip"
-      sha256 "0f4c7b386e0b789850308a5d665e94251d875db3b5093eaefbfe72e85a61fee3"
+      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1/mdviewer_0.1.1_linux_arm.zip"
+      sha256 "02679760eb0c570d027a489d2bb95d59511063ecf8c754568f3efb64288cf45e"
 
       def install
         bin.install "mdviewer"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1-alpha.2/mdviewer_0.1.1-alpha.2_linux_amd64.zip"
-      sha256 "1b3c6375fd4c0270ed3678c730b080c766a706887224a41139a1010a5ce15557"
+      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1/mdviewer_0.1.1_linux_amd64.zip"
+      sha256 "4184fe5601f21b56fd7c63437a0f2f636c5412bddfe0b8ce7e3337272633ee42"
+
+      def install
+        bin.install "mdviewer"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/noborus/mdviewer/releases/download/v0.1.1/mdviewer_0.1.1_linux_arm64.zip"
+      sha256 "937232bec97d1f3af06fa8e6e50550459a3660eb4f550cf22abf05bd3330428e"
 
       def install
         bin.install "mdviewer"
